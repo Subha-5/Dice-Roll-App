6455bdbaf2067bc761f4b651491778ee9aff5154
Subha Sadhu <subhasadhu5@gmail.com>
2023-04-27 02:12:11 +0530

    building stateful widgets and setState

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:first_app/dice_roller.dart';
+
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
@@ -15,10 +17,6 @@ class GradientContainer extends StatelessWidget {
   final Color color1;
   final Color color2;
 
-  void rollDice() {
-    // ...
-  }
-
   @override
   Widget build(context) {
     return Container(
@@ -29,32 +27,8 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/dice-2.png',
-              width: 200,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                /*padding: const EdgeInsets.only(
-                  top: 28,
-                ),*/
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(
-                  fontSize: 28,
-                ),
-              ),
-              child: const Text('Roll Dice'),
-            ),
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
