e4506fb4e06b3ebc521510817db13608b80790ca
Subha Sadhu <subhasadhu5@gmail.com>
2023-04-27 01:12:38 +0530

    Adding button to roll dice and applying padding with styles

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
-const String text = 'Hello World!';
 
 class GradientContainer extends StatelessWidget {
   // const GradientContainer({key}) : super(key: key);
@@ -16,6 +15,10 @@ class GradientContainer extends StatelessWidget {
   final Color color1;
   final Color color2;
 
+  void rollDice() {
+    // ...
+  }
+
   @override
   Widget build(context) {
     return Container(
@@ -27,9 +30,30 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Image.asset(
-          'assets/images/dice-2.png',
-          width: 200,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/dice-2.png',
+              width: 200,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                /*padding: const EdgeInsets.only(
+                  top: 28,
+                ),*/
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                  fontSize: 28,
+                ),
+              ),
+              child: const Text('Roll Dice'),
+            ),
+          ],
         ),
       ),
     );
